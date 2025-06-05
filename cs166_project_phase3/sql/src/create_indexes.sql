c1b7a3956ed022cbb27886355aef670ac752564a
Ridhima Inukurti <rinuk001@xe-10.cs.ucr.edu>
2025-06-04 22:46:40 -0700

    merged

--- cs166_project_phase3/sql/src/create_indexes.sql
+++ cs166_project_phase3/sql/src/create_indexes.sql
@@ -3,34 +3,16 @@ DROP INDEX IF EXISTS idx_flightnumber_flight;
 DROP INDEX IF EXISTS idx_flightnumber_schedule;
 DROP INDEX IF EXISTS idx_flightnumber_flightinstance;
 DROP INDEX IF EXISTS idx_flightdate_flightinstance;
-<<<<<<< HEAD
-
-DROP INDEX IF EXISTS idx_reservationid_reservation;
-DROP INDEX IF EXISTS idx_customerid_reservation;
-DROP INDEX IF EXISTS idx_customerid_customer;
-
-DROP INDEX IF EXISTS idx_technicianid_repair;
-DROP INDEX IF EXISTS idx_planeid_repair;
-DROP INDEX IF EXISTS idx_repairdate_repair;
-
-=======
 DROP INDEX IF EXISTS idx_reservationid_reservation;
 DROP INDEX IF EXISTS idx_customerid_reservation;
 DROP INDEX IF EXISTS idx_customerid_customer;
 DROP INDEX IF EXISTS idx_technicianid_repair;
 DROP INDEX IF EXISTS idx_planeid_repair;
 DROP INDEX IF EXISTS idx_repairdate_repair;
->>>>>>> refs/remotes/origin/main
 DROP INDEX IF EXISTS idx_planeid_plane;
 DROP INDEX IF EXISTS idx_pilotid_pilot;
 DROP INDEX IF EXISTS idx_pilotid_maintenancerequest;
 
-<<<<<<< HEAD
-DROP INDEX IF EXISTS idx_username_login;
-DROP INDEX IF EXISTS idx_userid_login;
-
-=======
->>>>>>> refs/remotes/origin/main
 -- Create Indexes for Flight-related tables
 CREATE INDEX idx_flightnumber_flight ON Flight(FlightNumber);
 CREATE INDEX idx_flightnumber_schedule ON Schedule(FlightNumber);
@@ -54,12 +36,5 @@ CREATE INDEX idx_planeid_plane ON Plane(PlaneID);
 CREATE INDEX idx_pilotid_pilot ON Pilot(PilotID);
 
 -- Create Index for MaintenanceRequest table
-<<<<<<< HEAD
 CREATE INDEX idx_pilotid_maintenancerequest ON MaintenanceRequest(PilotID);
 
--- Create Indexes for Login table
-CREATE INDEX idx_username_login ON Login(userName);
-CREATE INDEX idx_userid_login ON Login(userID);
-=======
-CREATE INDEX idx_pilotid_maintenancerequest ON MaintenanceRequest(PilotID);
->>>>>>> refs/remotes/origin/main
